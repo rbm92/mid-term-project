e97200a76f912e98d62019bf97883ee73bd60dd9
Ricard Bargallo <ricard.bargallo.m@gmail.com>
2022-03-08 18:06:26 +0100

    08-03-22

--- js/app.js
+++ js/app.js
@@ -5,11 +5,13 @@ function hambMenu() {
 }
 
 // Fetching Projects
+let fetchUrlProj = "http://localhost:8000/projects";
+let fetchUrlOtherProj = "http://localhost:8000/other-projects";
+
 async function fetchProj() {
-    let fetchUrl = "http://localhost:8000/projects";
     let projectImg = document.querySelectorAll('.img-project');
     for (let i = 0; i < projectImg.length; i++) {
-        fetch(fetchUrl)
+        fetch(fetchUrlProj)
             .then(response => response.json())
             .then(data => projectImg[i].setAttribute('src', data[i].img))
             .catch(err => console.log(err));
@@ -17,7 +19,7 @@ async function fetchProj() {
 
     let projectTitle = document.querySelectorAll('.project-title');
     for (let i = 0; i < projectTitle.length; i++) {
-        fetch(fetchUrl)
+        fetch(fetchUrlProj)
             .then(response => response.json())
             .then(data => projectTitle[i].innerText = data[i].title)
             .catch(err => console.log(err));
@@ -25,7 +27,7 @@ async function fetchProj() {
 
     let projectDesc = document.querySelectorAll('.project-description');
     for (let i = 0; i < projectDesc.length; i++) {
-        fetch(fetchUrl)
+        fetch(fetchUrlProj)
             .then(response => response.json())
             .then(data => projectDesc[i].innerText = data[i].description)
             .catch(err => console.log(err));
@@ -33,7 +35,7 @@ async function fetchProj() {
 
     let projectCont = document.querySelectorAll('.project-text');
     for (let i = 0; i < projectCont.length; i++) {
-        fetch(fetchUrl)
+        fetch(fetchUrlProj)
             .then(response => response.json())
             .then(data => projectCont[i].innerText = data[i].content)
             .catch(err => console.log(err));
@@ -44,10 +46,9 @@ fetchProj();
 
 // Fetching related projects
 async function fetchOtherProj() {
-    let fetchUrl = "http://localhost:8000/other-projects";
     let projectImg = document.querySelectorAll('.img-project');
     for (let i = 0; i < projectImg.length; i++) {
-        fetch(fetchUrl)
+        fetch(fetchUrlOtherProj)
             .then(response => response.json())
             .then(data => projectImg[i].setAttribute('src', data[i].img))
             .catch(err => console.log(err));
@@ -55,7 +56,7 @@ async function fetchOtherProj() {
 
     let projectTitle = document.querySelectorAll('.other-project-title');
     for (let i = 0; i < projectTitle.length; i++) {
-        fetch(fetchUrl)
+        fetch(fetchUrlOtherProj)
             .then(response => response.json())
             .then(data => projectTitle[i].innerText = data[i].title)
             .catch(err => console.log(err));
@@ -63,7 +64,7 @@ async function fetchOtherProj() {
 
     let projectDesc = document.querySelectorAll('.other-project-description');
     for (let i = 0; i < projectDesc.length; i++) {
-        fetch(fetchUrl)
+        fetch(fetchUrlOtherProj)
             .then(response => response.json())
             .then(data => projectDesc[i].innerText = data[i].description)
             .catch(err => console.log(err));
@@ -71,7 +72,7 @@ async function fetchOtherProj() {
 
     let projectCont = document.querySelectorAll('.other-project-text');
     for (let i = 0; i < projectCont.length; i++) {
-        fetch(fetchUrl)
+        fetch(fetchUrlOtherProj)
             .then(response => response.json())
             .then(data => projectCont[i].innerText = data[i].content)
             .catch(err => console.log(err));
@@ -82,8 +83,10 @@ fetchOtherProj();
 
 // Contact Form Validation
 let form = document.querySelector('.form-contact-form');
+let fetchUrlForm = "http://localhost:8000/messages";
+let validation = true;
 
-function saveData(event) {
+async function saveData(event) {
     event.preventDefault(); // stop the form submitting
 
     let data = {
@@ -98,35 +101,55 @@ function saveData(event) {
     divValidation.innerHTML = ''; // reset previous validations
 
     if (!data.fullname) {
-        divValidation.innerHTML += `<p class="validation-message">Name field must not be empty</p>`;
+        divValidation.innerHTML += `<p class="validation wrong">Name field must not be empty</p>`;
+        validation = false;
     }
 
     if (!data.email) {
-        divValidation.innerHTML += `<p class="validation-message">Email field must not be empty</p>`;
+        divValidation.innerHTML += `<p class="validation wrong">Email field must not be empty</p>`;
+        validation = false;
     }
 
     if (data.phone.length < 9) {
-        divValidation.innerHTML += `<p class="validation-message">Phone number field must contain at least 9 characters</p>`;
+        divValidation.innerHTML += `<p class="validation wrong">Phone number field must contain at least 9 characters</p>`;
+        validation = false;
     }
 
-    if (data.message.length < 50) {
-        divValidation.innerHTML += `<p class="validation-message">Message must contain at least 50 characters</p>`;
+    if (data.message.length < 30) {
+        divValidation.innerHTML += `<p class="validation wrong">Message must contain at least 30 characters</p>`;
+        validation = false;
     }
 
-    else {
+    if (!validation) return; // no guardar los datos
 
-        // Saving Contact Form Data
-        let fetchUrl = "http://localhost:8000/messages";
+    // Saving Contact Form Data
+    fetch(fetchUrlForm, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(results => results.json());
 
-        fetch(fetchUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-            .then(results => results.json());
-    }
+
+    divValidation.innerHTML += `<p class="validation right">Your contact details were sent succesfully</p>`;
+    return false;
 }
 
 form.addEventListener('submit', saveData);
+
+// Fetching Projects
+
+function projPage() {
+    let projectWrap = document.querySelectorAll('.project-wrap');
+    for (let i = 0; i < projectWrap.length; i++) {
+        fetchProjPage = `http://localhost:8000/projects/{i}`;
+        fetch(fetchProjPage)
+            .then(response => response.json())
+            .then(data => projectWrap[i].setAttribute('href', data[i].id))
+            .catch(err => console.log(err));
+    }
+}
+
+addEventListener('onclick', projPage);
